5ce3bf21c8f1b8c700dc9a71dbaa50178ff47cbf
potatofish <5317855+potatofish@users.noreply.github.com>
2023-06-10 14:19:08 -0700

    works across 292 inputs, reporting 144 errors

--- field_definition.py
+++ field_definition.py
@@ -3,15 +3,17 @@ class FieldDefinition:
         self.name = name
         self.size = size
 
-def compare_actual_to_expected(field_label, actual, expected, warn_only=False, cb=lambda x: x):
-    comp_result = actual == expected
-    error_label = "Error"
-    if warn_only:
-        error_label = "Warning"
 
-    if not comp_result:
-        print(f"  ➡  {field_label}: ✖")
-        print(f"     {error_label}: Invalid {field_label} [Actual, Expected] [{cb(actual)}, {cb(expected)}]")
+LOG_LEVEL_QUIET = 0
+LOG_LEVEL_NORMAL = 1
+LOG_LEVEL_VERBOSE =2
+def compare_actual_to_expected(field_label, actual, expected, log_level=LOG_LEVEL_QUIET, cb=lambda x: x):
+    comp_result = actual == expected
+    log = ""
+    if not comp_result and log_level != LOG_LEVEL_QUIET:
+        log += f"  ➡  {field_label}: ✖\n"
+        if log_level == LOG_LEVEL_VERBOSE:
+            log += f"      ➡  Invalid {field_label} [Actual, Expected] [{cb(actual)}, {cb(expected)}]\n"
     else:
-        print(f"  ➡  {field_label}: ✔\t[{cb(actual)}:{type(actual)}]")
-    return comp_result
+        log += f"  ➡  {field_label}: ✔\t[{cb(actual)}:{type(actual)}]\n"
+    return [comp_result,log]
